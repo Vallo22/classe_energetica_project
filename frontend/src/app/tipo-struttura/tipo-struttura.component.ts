a71a3000923caaa85d585491e669c3a6d2cfadba
Daniele Vallorani <61566266+Vallo22@users.noreply.github.com>
2021-01-25 23:12:26 +0100

    modal img

--- frontend/src/app/tipo-struttura/tipo-struttura.component.ts
+++ frontend/src/app/tipo-struttura/tipo-struttura.component.ts
@@ -26,9 +26,61 @@ export class TipoStrutturaComponent implements OnInit {
   selectedIndexDue: number;
   selectArr: number;
 
+  imgA1 = false
+  imgA2 = false
+  imgA3 = false
+  imgA4 = false
+  imgA5 = false
+  imgA6 = false
+  imgA7 = false
+  imgA8 = false
   imgB1 = false
+  imgB2 = false
+  imgB3 = false
+  imgB4 = false
+  imgB5 = false
+  imgB6 = false
+  imgB7 = false
+  imgB8 = false
+  imgB9 = false
+  imgB10 = false
+  imgB11 = false
+  imgB12 = false
+  imgB13 = false
+  imgB14 = false
+  imgC1 = false
+  imgC2 = false
+  imgC3 = false
+  imgC4 = false
+  imgC5 = false
+  imgC6 = false
+  imgC7 = false
+  imgC8 = false
+  imgC9 = false
+  imgC10 = false
+  imgC11 = false
+  imgD1 = false
+  imgD2 = false
+  imgD3 = false
+  imgD4 = false
+  imgE1 = false
+  imgE2 = false
+  imgE3 = false
+  imgE4 = false
+  imgE5 = false
+  imgE6 = false
+  imgE7 = false
+  imgE8 = false
+  imgE9 = false
+  imgE10 = false
+  imgE11 = false
+  imgE12 = false
+  imgE13 = false
+  imgF1 = false
+  imgF2 = false
+  imgF3 = false
+  imgF4 = false
   
-
   constructor(
     private service: ElementiStrutturaService,
     private serviceAssociazione: AssociazioneInterventoService
@@ -102,9 +154,6 @@ export class TipoStrutturaComponent implements OnInit {
     if(codice == "B1"){
       this.imgB1 = true
     }
-  }
-
-  apriPopup() {
   }
 
 }
