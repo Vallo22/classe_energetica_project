f9a4de64283e7f1858e237dc2c00ff65b645c2f2
Daniele Vallorani <61566266+Vallo22@users.noreply.github.com>
2021-02-09 14:18:33 +0100

    Update mcdm.component.ts

--- frontend/src/app/mcdm/mcdm.component.ts
+++ frontend/src/app/mcdm/mcdm.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 import { AssociazioneIntervento } from '../classi-servizi/classes/associazione-intervento';
 import { CaratteristicheQualitative } from '../classi-servizi/classes/caratteristiche-qualitative';
-import { StrutturaTre } from '../classi-servizi/classes/strutture/struttura-tre';
 import { ElementiStrutturaService } from '../classi-servizi/service/elementi-struttura.service';
-import { RisultatoSelezioneService } from '../classi-servizi/service/risultato-selezione.service';
 
 @Component({
   selector: 'app-mcdm',
@@ -14,19 +11,13 @@ import { RisultatoSelezioneService } from '../classi-servizi/service/risultato-s
 export class McdmComponent {
 
   constructor(
-    private risultatoSelezione: RisultatoSelezioneService,
-    private router: Router,
     private service: ElementiStrutturaService
-  ) {
-  }
+  ) { }
 
-  result: boolean = false
   ponderazione: number[] = [0, 0, 0, 0, 0, 0]
-  strutturaTre: StrutturaTre[];
-  variabileIntervento: AssociazioneIntervento[];
+  variabileIntervento: AssociazioneIntervento[]
   caratteristiche: CaratteristicheQualitative
   car: CaratteristicheQualitative[]
-  totale: number[];
   modCos: number
   effic: number
   supInton: number
@@ -35,35 +26,7 @@ export class McdmComponent {
   revers: number
   semplCant: number
   esigIngom: number
-  carQual: number
-  intervento: number
-  strutturaAssociazione: number
-  toggleSuperficie: number
-  buttonIntervento: boolean = false
-  a: boolean = false
-  selectedElement1: any
-  selectedElement2: any
-  deltaPunteggioFinale: number = 0
-  vulClass: number;
-  risk: String
-  pam: String
-  punteggio: number
-  soglia: number
-  emsType: Number
-  arraySelezionati: AssociazioneIntervento[] = []
-  selectedElement: AssociazioneIntervento
-  counterClickCheck: number = 0
-  punteggioPassaggioClasse: number
-  punteggioDiVul: number = 0
-  punteggioPassaggioClasseAggiornato: number = 0
-  contatoreVolte: number
-  interventoSingolo: number
-  bottoneVisibile: boolean = false
-  bottoneVisibile1: boolean = false
-  ugualeA0: boolean = false
   tipo_superficie: number = 0
-
-
 
   ngOnInit() {
     this.caratteristiche = window.history.state.caratteristiche
@@ -82,43 +45,43 @@ export class McdmComponent {
       for (const i in t.varianti) {
         this.modCos = t.modicitaDiCosto[i] * this.ponderazione[0]
         this.effic = t.efficacia[i] * this.ponderazione[1]
-        if(this.tipo_superficie == 1) {
+        if (this.tipo_superficie == 1) {
           this.supInton = t.supIntonacate[i] * this.ponderazione[2]
           this.revers = t.reversibilita[i] * this.ponderazione[3]
           this.semplCant = t.semplicitaDiCantiere[i] * this.ponderazione[4]
           this.esigIngom = t.esiguitaDiIngombro[i] * this.ponderazione[5]
-          if(this.supInton === 0){
+          if (this.supInton === 0) {
             t.totale.push(0)
           } else {
             t.totale.push(this.modCos + this.effic + this.supInton + this.revers + this.semplCant + this.esigIngom)
           }
-        } 
-        else if(this.tipo_superficie == 2) {
+        }
+        else if (this.tipo_superficie == 2) {
           this.supEv = t.supEvIi[i] * this.ponderazione[2]
           this.revers = t.reversibilita[i] * this.ponderazione[3]
           this.semplCant = t.semplicitaDiCantiere[i] * this.ponderazione[4]
           this.esigIngom = t.esiguitaDiIngombro[i] * this.ponderazione[5]
-          if(this.supEv === 0){
+          if (this.supEv === 0) {
             t.totale.push(0)
           } else {
             t.totale.push(this.modCos + this.effic + this.supEv + this.revers + this.semplCant + this.esigIngom)
-          }        
-        } 
-        else if(this.tipo_superficie == 3) {
+          }
+        }
+        else if (this.tipo_superficie == 3) {
           this.supIv = t.supIvEi[i] * this.ponderazione[2]
           this.revers = t.reversibilita[i] * this.ponderazione[3]
           this.semplCant = t.semplicitaDiCantiere[i] * this.ponderazione[4]
           this.esigIngom = t.esiguitaDiIngombro[i] * this.ponderazione[5]
-          if(this.supIv === 0){
+          if (this.supIv === 0) {
             t.totale.push(0)
           } else {
             t.totale.push(this.modCos + this.effic + this.supIv + this.revers + this.semplCant + this.esigIngom)
-          }         
+          }
         }
       }
       console.log("Totale: ", t.totale);
     })
-    this.massimoNumero() 
+    this.massimoNumero()
   }
 
   massimoNumero() {
@@ -137,9 +100,9 @@ export class McdmComponent {
             maxIndex = parseInt(index)
           }
           if (interventi.totale[index] < min) {
-              min = interventi.totale[index]
-              minIntervento = interventi
-              minIndex = parseInt(index)
+            min = interventi.totale[index]
+            minIntervento = interventi
+            minIndex = parseInt(index)
           }
         }
       }
