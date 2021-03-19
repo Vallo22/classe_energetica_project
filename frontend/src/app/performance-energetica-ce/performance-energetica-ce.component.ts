3d4dc60c41189eda563966bfe40c8062d4fa044e
Daniele Vallorani <61566266+Vallo22@users.noreply.github.com>
2021-03-19 13:09:21 +0100

    prova totale

--- frontend/src/app/performance-energetica-ce/performance-energetica-ce.component.ts
+++ frontend/src/app/performance-energetica-ce/performance-energetica-ce.component.ts
@@ -13,6 +13,8 @@ export class PerformanceEnergeticaCeComponent implements OnInit {
   indicatore: IndicatoreCe[] = [];
   classeIndicatore: ClasseIndicatoreCe[];
   selezione: number[] = [];
+  msg1: boolean = false
+  totalePunteggio: number
 
   constructor(
     private service: PerformanceEnergeticaCeService
@@ -27,6 +29,29 @@ export class PerformanceEnergeticaCeComponent implements OnInit {
       this.classeIndicatore = data;
       console.log(this.classeIndicatore)
     })
+    console.log("rapportos/v: " + this.selezione[0])
   }
+
+  outputTotale() {
+    this.msg1= true
+    let totalePunteggio = 0
+    this.selezione.forEach((function(value) {
+      for(const key in this.edificio) {
+        const list = this.edificio[key]
+        list.forEach(function(ed) {
+          if(ed.id === value) {
+            totalePunteggio = totalePunteggio + parseInt(ed.punteggio)
+          }
+        })
+        }
+      }).bind(this))
+      const selezionati = {}
+      for(const value of this.selezione) {
+        selezionati[value] = true
+      }
+      this.totalePunteggio = totalePunteggio
+      console.log("rapportos/v: " + this.selezione[0])
+    }
+    
 
 }
