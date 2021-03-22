525aca0f1c487d9d3a8d779bfd36ff6d936fe127
Daniele Vallorani <61566266+Vallo22@users.noreply.github.com>
2021-03-22 15:07:40 +0100

    prove sum

--- frontend/src/app/performance-energetica-ce/performance-energetica-ce.component.ts
+++ frontend/src/app/performance-energetica-ce/performance-energetica-ce.component.ts
@@ -10,12 +10,12 @@ import { PerformanceEnergeticaCeService } from '../classes-services/services/per
 })
 export class PerformanceEnergeticaCeComponent implements OnInit {
 
-  indicatore: IndicatoreCe[] = [];
-  classeIndicatore: ClasseIndicatoreCe[];
-  selezione: number[] = [];
+  indicatore: IndicatoreCe[]
+  classeIndicatore: ClasseIndicatoreCe[]
+  selezione: number[]
   msg1: boolean = false
   totalePunteggio: number
-
+  
   constructor(
     private service: PerformanceEnergeticaCeService
   ) { }
@@ -29,44 +29,16 @@ export class PerformanceEnergeticaCeComponent implements OnInit {
       this.classeIndicatore = data;
       console.log(this.classeIndicatore)
     })
-    this.classeIndicatore.forEach(element => {
-      this.indicatore.push(element.indicatore);
-      this.selezione.push(element.id)
-    })
-    this.cleanArray()
   }
 
   outputTotale() {
-    this.msg1= true
-    let totalePunteggio = 0
-    this.selezione.forEach((function(value) {
-      for(const key in this.edificio) {
-        const list = this.edificio[key]
-        list.forEach(function(ed) {
-          if(ed.id === value) {
-            totalePunteggio = totalePunteggio + parseInt(ed.punteggio)
-          }
-        })
-        }
-      }).bind(this))
-      const selezionati = {}
-      for(const value of this.selezione) {
-        selezionati[value] = true
-      }
-      this.totalePunteggio = totalePunteggio
-    }
-    
-    cleanArray() {
-      const indicatori: number[] = []
-      this.indicatore.forEach(el => {
-        indicatori.push(el.id)
-      })
-      const unico = new Set(indicatori)
-      const indicatoreTemp: IndicatoreCe[] = []
-      unico.forEach(u => {
-        indicatoreTemp.push(this.indicatore.find(qE => qE.id == u))
-      })
-      this.indicatore = indicatoreTemp
-    }
+    //this.totalePunteggio = this.selezione.map((option: ClasseIndicatoreCe) => option.punteggio).reduce((punteggioA: number, punteggioB: number) => punteggioA + punteggioB)
+
+    //const totale = this.classeIndicatore.filter(c => this.selezione.includes(c.id)).reduce((a,c) => a + c.punteggio, 0)
+    //this.totalePunteggio = totale
+    this.msg1 = true
+    console.log(this.totalePunteggio)
+  }
+
     
 }
