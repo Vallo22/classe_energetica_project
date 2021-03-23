a817aa975b6636195a6f8ec51ad8d5b2c519c695
Daniele Vallorani <61566266+Vallo22@users.noreply.github.com>
2021-03-23 18:37:01 +0100

    fix edificio reale e ideale

--- frontend/src/app/performance-energetica-ce/performance-energetica-ce.component.ts
+++ frontend/src/app/performance-energetica-ce/performance-energetica-ce.component.ts
@@ -16,12 +16,16 @@ export class PerformanceEnergeticaCeComponent implements OnInit {
   classeIndicatore: ClasseIndicatoreCe[]
   indicatoreNon: IndicatoreNonCe[]
   classeIndicatoreNon: ClasseIndicatoreNonCe[]
+
   selezione: number[] = []
   selezione1: number[] = []
   msg1: boolean = false
-  totalePunteggio: number
-  epgl: number
   scelta: boolean = true
+
+  totaleEdificioReale: number
+  epglEdificioReale: number
+  totaleEdificioIdeale: number
+  epglEdificioIdeale: number
   
   constructor(
     private service: PerformanceEnergeticaCeService
@@ -53,27 +57,55 @@ export class PerformanceEnergeticaCeComponent implements OnInit {
   }
 
   outputTotale() {
-    const totale = this.classeIndicatore
+    const totaleEdificioReale = this.classeIndicatore
     .filter(c => this.selezione.map(m => +m).includes(c.id))
     .reduce((a, c) => a + c.punteggio, 0);
-    this.totalePunteggio = totale
-    const epgl = ((this.totalePunteggio + 1.68875) / 1.00437 )
-    this.epgl = epgl
+    this.totaleEdificioReale = totaleEdificioReale
+
+    const epglEdificioReale = ((this.totaleEdificioReale + 1.68875) / 1.00437 )
+    this.epglEdificioReale = epglEdificioReale
+
+    const parziale = this.classeIndicatore
+    .filter(c => this.selezione.map(m => +m).includes(c.id))
+    .slice(0,5)
+    .reduce((a, c) => a + c.punteggio, 0);
+    this.totaleEdificioIdeale = parziale + 0.16 + 0.1 + 0.04 + 0.04 + 0.03 + 0.35 + 0.09 + 0.03
+
+    const epglEdificioIdeale = ((this.totaleEdificioIdeale + 2.1079) / 1.2311 )
+    this.epglEdificioIdeale = epglEdificioIdeale
+
     this.msg1 = true
-    console.log('totale: ' + this.totalePunteggio)
-    console.log('epgl: ' + this.epgl)
+    console.log('EDIFICIO CON CLIMATIZZAZIONE ESTIVA')
+    console.log('totale ed. reale: ' + this.totaleEdificioReale)
+    console.log('epgl ed. reale: ' + this.epglEdificioReale)
+    console.log('totale ed. ideale' + this.totaleEdificioIdeale)
+    console.log('epgl ed. ideale' + this.epglEdificioIdeale)
   }
 
   outputTotaleNon() {
-    const totale = this.classeIndicatoreNon
+    const totaleEdificioReale = this.classeIndicatoreNon
     .filter(c => this.selezione1.map(m => +m).includes(c.id))
     .reduce((a, c) => a + c.punteggio, 0);
-    this.totalePunteggio = totale
-    const epgl = ((this.totalePunteggio + 1.12725) / 0.7474 )
-    this.epgl = epgl
+    this.totaleEdificioReale = totaleEdificioReale
+
+    const epglEdificioReale = ((this.totaleEdificioReale + 1.12725) / 0.7474 )
+    this.epglEdificioReale = epglEdificioReale
+
+    const parziale = this.classeIndicatoreNon
+    .filter(c => this.selezione1.map(m => +m).includes(c.id))
+    .slice(0,5)
+    .reduce((a, c) => a + c.punteggio, 0);
+    this.totaleEdificioIdeale = parziale + 0.16 + 0.1 + 0.04 + 0.04 + 0.03 + 0.35 + 0.14
+
+    const epglEdificioIdeale = ((this.totaleEdificioIdeale + 2.1079) / 1.2311 )
+    this.epglEdificioIdeale = epglEdificioIdeale
+
     this.msg1 = true
-    console.log('totale: ' + this.totalePunteggio)
-    console.log('epgl: ' + this.epgl)
+    console.log('EDIFICIO SENZA CLIMATIZZAZIONE ESTIVA')
+    console.log('totale ed. reale: ' + this.totaleEdificioReale)
+    console.log('epgl ed. reale: ' + this.epglEdificioReale)
+    console.log('totale ed. ideale' + this.totaleEdificioIdeale)
+    console.log('epgl ed. ideale' + this.epglEdificioIdeale)
   }
 
     
