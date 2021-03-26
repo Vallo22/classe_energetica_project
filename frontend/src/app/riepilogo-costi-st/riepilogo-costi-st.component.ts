4e826c972509c7078ea4cf921e2c7cff88062a06
Daniele Vallorani <61566266+Vallo22@users.noreply.github.com>
2021-03-26 16:29:38 +0100

    fix vari + fix danni ipotizzati

--- frontend/src/app/riepilogo-costi-st/riepilogo-costi-st.component.ts
+++ frontend/src/app/riepilogo-costi-st/riepilogo-costi-st.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AssociazioneInterventoSt } from '../classes-services/classes/associazione-intervento-st';
-import { InterventoSt } from '../classes-services/classes/intervento-st';
+import { CostoAAB } from '../classes-services/classes/damage/costo-aab';
+import { CostoC } from '../classes-services/classes/damage/costo-c';
+import { CostoDE } from '../classes-services/classes/damage/costo-de';
+import { CostoFG } from '../classes-services/classes/damage/costo-fg';
 import { RisultatoSelezioneStService } from '../classes-services/services/risultato-selezione-st.service';
 
 @Component({
@@ -14,20 +17,51 @@ export class RiepilogoCostiStComponent implements OnInit {
     private risultatoSelezione: RisultatoSelezioneStService
   ) { }
 
-  interventiDaMostrare: InterventoSt[] = []
   interventi: AssociazioneInterventoSt[] = []
-  elementoSelezionato: AssociazioneInterventoSt[] = []
-  prezzoSingoloIntervento: number[] = []
-  totale: number = 0
   risk: string
   soglia: number
-  risultatoDivisione: number
-  costoDiRiparazione: number
   sommaPacchettoInterventi: number
-  valoreCosto: number
-  numeroUtente: number
-  costoRiparazione: number
   tipo_superficie: number = 0
+  totale: number = 0
+  risultatoDivisione: number
+
+  selezione: number
+  inputUtente: number
+  costoDiRiparazione: number = 0
+  
+  costoAAB: CostoAAB[] = [
+    {id: 1, nome: "D2", costo: 240},
+    {id: 2, nome: "D3", costo: 840},
+    {id: 3, nome: "D4", costo: 1200},
+    {id: 4, nome: "D5", costo: 1200}
+  ]
+
+  costoFG: CostoFG[] = [
+    {id: 1, nome: "D0", costo: 840},
+    {id: 2, nome: "D1", costo: 840},
+    {id: 3, nome: "D2", costo: 1080},
+    {id: 4, nome: "D3", costo: 1200},
+    {id: 5, nome: "D4", costo: 1200},
+    {id: 6, nome: "D5", costo: 1200}
+  ]
+
+  costoC: CostoC[] = [
+    {id: 1, nome: "D0", costo: 240},
+    {id: 2, nome: "D1", costo: 240},
+    {id: 3, nome: "D2", costo: 600},
+    {id: 4, nome: "D3", costo: 840},
+    {id: 5, nome: "D4", costo: 1200},
+    {id: 6, nome: "D5", costo: 1200}
+  ]
+
+  costoDE: CostoDE[] = [
+    {id: 1, nome: "D0", costo: 600},
+    {id: 2, nome: "D1", costo: 600},
+    {id: 3, nome: "D2", costo: 840},
+    {id: 4, nome: "D3", costo: 1080},
+    {id: 5, nome: "D4", costo: 1200},
+    {id: 6, nome: "D5", costo: 1200}
+  ]
 
   ngOnInit() {
     this.sommaPacchettoInterventi = window.history.state.sommaPacchettoInterventi
@@ -38,18 +72,10 @@ export class RiepilogoCostiStComponent implements OnInit {
       this.interventi.push(z)
       console.log("interventi: ", this.interventi)
     })
-    this.selezionaIntervento()
-    console.log(this.dannoIpotizzato)
-    this.calcolaCostoRiparazione()
-  }
-
-  selezionaIntervento() {
-    this.interventi.forEach(z => {
-      this.elementoSelezionato.push(z)
-    })
   }
 
   calcolaInvestimentoTotale(){
+    this.totale = 0
     this.interventi.forEach(z => {
       let pr: number
       if(z.tipo_superficie == 1) {
@@ -64,53 +90,8 @@ export class RiepilogoCostiStComponent implements OnInit {
     this.risultatoDivisione = this.totale/this.soglia
   }
 
-  dannoIpotizzato = [
-    { id: 1, valore: "D0", costo: this.valoreCosto },
-    { id: 2, valore: "D1", costo: this.valoreCosto },
-    { id: 3, valore: "D2", costo: this.valoreCosto },
-    { id: 4, valore: "D3", costo: this.valoreCosto },
-    { id: 5, valore: "D4", costo: this.valoreCosto },
-    { id: 6, valore: "D5", costo: this.valoreCosto }
-  ]
-
-  calcolaCostoRiparazione(){
-    if(this.risk == "A+" || this.risk == "A" || this.risk == "B"){
-      this.dannoIpotizzato[2].costo = 240
-      this.dannoIpotizzato[3].costo = 840
-      this.dannoIpotizzato[4].costo = 1200
-      this.dannoIpotizzato[5].costo = 1200
-    }
-    else if(this.risk == "F" || this.risk == "G"){
-      this.dannoIpotizzato[0].costo = 840
-      this.dannoIpotizzato[1].costo = 840
-      this.dannoIpotizzato[2].costo = 1080
-      this.dannoIpotizzato[3].costo = 1200
-      this.dannoIpotizzato[4].costo = 1200
-      this.dannoIpotizzato[5].costo = 1200
-    }
-    else if(this.risk == "C"){
-      this.dannoIpotizzato[0].costo = 240
-      this.dannoIpotizzato[1].costo = 240
-      this.dannoIpotizzato[2].costo = 600
-      this.dannoIpotizzato[3].costo = 840
-      this.dannoIpotizzato[4].costo = 1200
-      this.dannoIpotizzato[5].costo = 1200
-    }
-    else if(this.risk == "D" || this.risk == "E"){
-      this.dannoIpotizzato[0].costo = 600
-      this.dannoIpotizzato[1].costo = 600
-      this.dannoIpotizzato[2].costo = 840
-      this.dannoIpotizzato[3].costo = 1080
-      this.dannoIpotizzato[4].costo = 1200
-      this.dannoIpotizzato[5].costo = 1200
-    }
-  }
-
-  onChangeDannoIpotizzato(i: number){
-    this.costoDiRiparazione = 0
-    console.log(i)
-    console.log("numero utente: ",this.numeroUtente)
-      this.costoRiparazione = this.dannoIpotizzato[i - 1].costo * this.numeroUtente
+  mostraTotaleRiparazione() {
+    this.costoDiRiparazione = this.inputUtente * this.selezione
   }
 
 }
